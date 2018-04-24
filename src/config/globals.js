0971f1a122d633d221ed96f5bbc301bc173c0672
Hayden Crain <haydencrain@optusnet.com.au>
2018-04-24 13:05:49 +1000

    update api url

--- src/config/globals.js
+++ src/config/globals.js
@@ -1,4 +1,4 @@
 export default {
-  api: "https://maxn9yb5ia.execute-api.ap-southeast-2.amazonaws.com/api",
+  api: "https://0in9yvj2tc.execute-api.ap-southeast-2.amazonaws.com/api/",
   userId: "f41bd45f-59df-4383-a5ff-8c2151d37eec"
 }
